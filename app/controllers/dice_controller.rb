734fcd6771b1b12038ad4ab7abd3ad4de8c3de74
marszman <136658905+marszman@users.noreply.github.com>
2023-07-23 18:57:38 +0000

    rails - dynamic dice - complete

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,16 @@
 class DiceController < ApplicationController
   def play_game
+
+    @num_dice = params.fetch("number_of_dice").to_i
+    @num_sides = params.fetch("number_of_sides").to_i
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+    end
+
     render({ :template => "game_templates/display_game" })
+
   end
 end
